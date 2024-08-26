40693e1994be21092679f6e52dc921a76e0901b7
sirinya1601 <Ssirinya.sondilok@gmail.com>
2024-08-26 15:05:02 +0700

    install cypress and cucumber

---
+++ cypress/e2e/step_difinitions/login/login.js
@@ -0,0 +1,22 @@
+import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps';
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+});
+
+Given('Open Website {string}', (website) => {
+    cy.visit(website)
+})
+
+When('Input user {string} and password {string}', (user, password) => {
+    cy.get('.box-area-step-1 > .ant-input').type(user);
+    cy.get('.btn-login').click();
+    cy.get('.box-area-step-1 > .ant-input-affix-wrapper').type(password);
+    cy.get('.btn-login').click();
+})
+
+And('Click Button login', () => {
+    cy.get('.btn-login').click();
+})
